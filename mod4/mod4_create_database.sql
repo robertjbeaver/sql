145d41801460b8cd46b8397a6633fca292856cfc
robertjbeaver <robertjbeaver@gmail.com>
2018-03-07 14:27:24 -0500

    added mod4 create database

---
+++ mod4/mod4_create_database.sql
@@ -0,0 +1,25 @@
+DROP DATABASE IF EXISTS mod4;
+CREATE DATABASE mod4;
+USE mod4;
+
+
+CREATE TABLE Customer (
+  CustomerID	 INT 			PRIMARY KEY  AUTO_INCREMENT,
+  Name			 VARCHAR(50)    NOT NULL        
+  City           VARCHAR(50)    NOT NULL
+  State		     VARCHAR(2)     NOT NULL
+  Sales	         DECIMAL(14,2)  NOT NULL
+  Active		 BIT			
+);
+
+
+Insert Customer (Name, City, State, Sales, Active) Values ('Acme,inc.','Jersey City','NJ',14381891,1);
+Insert Customer (Name, City, State, Sales, Active) Values ('Widget Corp','Seattle','WA',97865829,1);
+Insert Customer (Name, City, State, Sales, Active) Values ('123 Warehousing','Los Angeles','CA',77503710,1);
+Insert Customer (Name, City, State, Sales, Active) Values ('DemoCompany','Columbus','OH',74647541,1);
+Insert Customer (Name, City, State, Sales, Active) Values ('Smith and Co.','Honolulu','HI',36046590,1);
+Insert Customer (Name, City, State, Sales, Active) Values ('Foo Bars','Fresno','CA',39912857,1);
+Insert Customer (Name, City, State, Sales, Active) Values ('ABC Telecom','Birmingham','AL',31149083,1);
+Insert Customer (Name, City, State, Sales, Active) Values ('Fake Brothers','Stockton','CA',31226191,1);
+Insert Customer (Name, City, State, Sales, Active) Values ('QWERTY Logistics','Lubbock','TX',17226905,1);
+Insert Customer (Name, City, State, Sales, Active) Values ('Demo, Inc.','Las Vegas','NV',69551557,1);
